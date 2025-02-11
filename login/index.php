b74431ff22e2e31f97deae6fcf2e686eca225043
Saibusu <Saibusu8888@gmail.com>
2025-02-11 16:33:33 +0800

    index.php修改

--- login/index.php
+++ login/index.php
@@ -1,26 +1,28 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-/*define() 函數定義一個常量。
-在設定以後，常量的值無法更改
-常量名不需要開頭的美元符號 ($)
-作用域不影響對常量的訪問
-常量值只能是字符串或數字*/
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+// Initialize the session
+session_start();
  
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-// 輸入中文也OK的編碼
-mysqli_query($link, 'SET NAMES utf8');
-
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-else{
-    return $link;
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;  //記得要跳出來，不然會重複轉址過多次
 }
 ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>登入介面</title>
+</head>
+<body>
+    <h1>Log In</h1>
+    <h2>你可以選擇登入或是註冊帳號~</h2>
+<form method="post" action="login.php">
+帳號：
+<input type="text" name="username"><br/><br/>
+密碼：
+<input type="password" name="password"><br><br>
+<input type="submit" value="登入" name="submit"><br><br>
+<a href="register.html">還沒有帳號？現在就註冊！</a>
+</form>
+</body>
+</html>
